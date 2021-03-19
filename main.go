ef0f1733d4d2885ec0e3bd37e78dce3347a68782
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-03-19 09:44:58 +0100

    wip

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"time"
 
@@ -14,13 +15,21 @@ import (
 )
 
 func traverse(ctx context.Context, node *cdp.Node, name string) error {
+	if node == nil {
+		return nil
+	}
 	fmt.Printf("[%d] %d /%s/%s %s %d\n", node.NodeID, node.NodeType, name, node.NodeName, node.NodeValue, node.ChildNodeCount)
 	quads, err := dom.GetContentQuads().WithNodeID(node.NodeID).Do(ctx)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("quads: %v\n", quads)
-	for _, n := range node.Children {
+	nodeParams, err := dom.DescribeNode().WithNodeID(node.NodeID).WithDepth(1).WithPierce(true).Do(ctx)
+	err = dom.RequestChildNodes(nodeParams.NodeID).WithDepth(-1).Do(ctx)
+	if err != nil {
+		return err
+	}
+	for _, n := range nodeParams.Children {
 		traverse(ctx, n, fmt.Sprintf("/%s/%s", name, node.NodeName))
 	}
 	return nil
@@ -33,10 +42,12 @@ func main() {
 	defer cancel()
 
 	// run
+	var ids []cdp.NodeID
 	err := chromedp.Run(ctx,
 		// set viewport
 		chromedp.EmulateViewport(780, 651),
 		chromedp.Navigate(`https://www.whatsmyua.info/?a`),
+		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			var err error
 			var root *cdp.Node
@@ -47,6 +58,16 @@ func main() {
 			if err != nil {
 				return err
 			}
+			// export html for debug
+			res, err := dom.GetOuterHTML().WithNodeID(root.NodeID).Do(ctx)
+			if err != nil {
+				return err
+			}
+			err = ioutil.WriteFile("layout.html", []byte(res), 0o644)
+			if err != nil {
+				return err
+			}
+
 			err = traverse(ctx, root, "")
 			if err != nil {
 				return err
