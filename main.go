d5fbc1dc169ffcbea1cbf1446a6f84ddb18db554
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-03-20 16:37:10 +0100

    write down dom in files

--- main.go
+++ main.go
@@ -22,12 +22,48 @@ const (
 	usageURL   = "the URL to be scrapped"
 )
 
+type ElementAnalysis struct {
+	filename    string
+	headerNodes []*cdp.Node
+}
+
+func (e *ElementAnalysis) getElementHeights(ctx context.Context) error {
+	var res string
+	for _, node := range e.headerNodes {
+		res += fmt.Sprintf("-------------------- %s ------------------ \n", node.NodeName)
+		res += fmt.Sprintf("Node: %d %s %s %s %s %+v %s %s\n",
+			node.NodeID,
+			node.Name,
+			node.Value,
+			node.NodeName,
+			node.NodeValue,
+			node.Attributes,
+			node.PartialXPath(),
+			node.FullXPath())
+		res += fmt.Sprintf("\tChildren: %+v \n", node.Children)
+		quads, err := dom.GetContentQuads().WithNodeID(node.NodeID).Do(ctx)
+		if err != nil {
+			res += fmt.Sprintf("\t%s\n", err.Error())
+			continue
+		}
+		res += fmt.Sprintf("\tPosition top/bottom vertical element position: [%d px.. %d px]\n", int(quads[0][1]), int(quads[0][5]))
+	}
+	err := ioutil.WriteFile(e.filename, []byte(res), 0o644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	start := time.Now()
 	// create context
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 	var url string
+	e := ElementAnalysis{
+		headerNodes: []*cdp.Node{},
+	}
 
 	flag.StringVar(&url, "url", defaultURL, usageURL)
 	flag.StringVar(&url, "u", defaultURL, usageURL+" (shorthand)")
@@ -35,113 +71,43 @@ func main() {
 	// run
 	fmt.Printf("Open URL %s\n", url)
 	var ids []cdp.NodeID
-	var headerNodes []*cdp.Node
 	err := chromedp.Run(ctx,
 		// set viewport
 		chromedp.EmulateViewport(780, 651),
 		chromedp.Navigate(url),
 		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
 		chromedp.ActionFunc(func(c context.Context) error {
+			e.filename = "780x651.dom"
 			// depth -1 for the entire subtree
 			// do your best to limit the size of the subtree
 			return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
 		}),
-		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &headerNodes, chromedp.ByQueryAll),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			for _, node := range headerNodes {
-				/*				fmt.Printf("-------------------- %s ------------------ \n", node.NodeName)
-								fmt.Printf("Node: %d %s %s %s %s %+v %s %s\n",
-									node.NodeID,
-									node.Name,
-									node.Value,
-									node.NodeName,
-									node.NodeValue,
-									node.Attributes,
-									node.PartialXPath(),
-									node.FullXPath())
-								fmt.Printf("\tChildren: %+v \n", node.Children)*/
-				quads, err := dom.GetContentQuads().WithNodeID(node.NodeID).Do(ctx)
-				if err != nil {
-					//					fmt.Printf("\t%s\n", err.Error())
-					continue
-				}
-				//fmt.Printf("\tPosition top/bottom vertical element position: [%d px.. %d px]\n", int(quads[0][1]), int(quads[0][5]))
-				if quads[0][1] > quads[0][5] {
-					continue
-				}
-			}
-			return nil
-		}),
+		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
+		chromedp.ActionFunc(e.getElementHeights),
 		// set viewport
 		chromedp.EmulateViewport(1280, 720),
 		chromedp.Navigate(url),
 		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
 		chromedp.ActionFunc(func(c context.Context) error {
+			e.filename = "1280x720.dom"
 			// depth -1 for the entire subtree
 			// do your best to limit the size of the subtree
 			return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
 		}),
-		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &headerNodes, chromedp.ByQueryAll),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			for _, node := range headerNodes {
-				/*				fmt.Printf("-------------------- %s ------------------ \n", node.NodeName)
-								fmt.Printf("Node: %d %s %s %s %s %+v %s %s\n",
-									node.NodeID,
-									node.Name,
-									node.Value,
-									node.NodeName,
-									node.NodeValue,
-									node.Attributes,
-									node.PartialXPath(),
-									node.FullXPath())
-								fmt.Printf("\tChildren: %+v \n", node.Children)*/
-				quads, err := dom.GetContentQuads().WithNodeID(node.NodeID).Do(ctx)
-				if err != nil {
-					//					fmt.Printf("\t%s\n", err.Error())
-					continue
-				}
-				//fmt.Printf("\tPosition top/bottom vertical element position: [%d px.. %d px]\n", int(quads[0][1]), int(quads[0][5]))
-				if quads[0][1] > quads[0][5] {
-					continue
-				}
-			}
-			return nil
-		}),
+		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
+		chromedp.ActionFunc(e.getElementHeights),
 		// set viewport
 		chromedp.EmulateViewport(1900, 1280),
 		chromedp.Navigate(url),
 		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
 		chromedp.ActionFunc(func(c context.Context) error {
+			e.filename = "1900x1280.dom"
 			// depth -1 for the entire subtree
 			// do your best to limit the size of the subtree
 			return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
 		}),
-		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &headerNodes, chromedp.ByQueryAll),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			for _, node := range headerNodes {
-				/*				fmt.Printf("-------------------- %s ------------------ \n", node.NodeName)
-								fmt.Printf("Node: %d %s %s %s %s %+v %s %s\n",
-									node.NodeID,
-									node.Name,
-									node.Value,
-									node.NodeName,
-									node.NodeValue,
-									node.Attributes,
-									node.PartialXPath(),
-									node.FullXPath())
-								fmt.Printf("\tChildren: %+v \n", node.Children)*/
-				quads, err := dom.GetContentQuads().WithNodeID(node.NodeID).Do(ctx)
-				if err != nil {
-					//					fmt.Printf("\t%s\n", err.Error())
-					continue
-				}
-				//fmt.Printf("\tPosition top/bottom vertical element position: [%d px.. %d px]\n", int(quads[0][1]), int(quads[0][5]))
-				if quads[0][1] > quads[0][5] {
-					continue
-				}
-			}
-			return nil
-		}),
+		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
+		chromedp.ActionFunc(e.getElementHeights),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			var err error
 			//			var root *cdp.Node
