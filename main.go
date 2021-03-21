b80dec59875c648dd9c2feea6983e46a985fb25a
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-03-21 08:46:09 +0100

    wip: ko

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 
@@ -14,6 +15,7 @@ import (
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/cdproto/emulation"
 	"github.com/chromedp/chromedp"
 )
 
@@ -23,8 +25,10 @@ const (
 )
 
 type ElementAnalysis struct {
-	filename    string
-	headerNodes []*cdp.Node
+	filename     string
+	screenWidht  int
+	screenHeigth int
+	headerNodes  []*cdp.Node
 }
 
 func (e *ElementAnalysis) getElementHeights(ctx context.Context) error {
@@ -40,7 +44,14 @@ func (e *ElementAnalysis) getElementHeights(ctx context.Context) error {
 			node.Attributes,
 			node.PartialXPath(),
 			node.FullXPath())
-		res += fmt.Sprintf("\tChildren: %+v \n", node.Children)
+		res += fmt.Sprintf("\tChildren (%d): %+v \n", node.ChildNodeCount, node.Children)
+		if node.ChildNodeCount == 0 {
+			html, err := dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+			if err != nil {
+				res += fmt.Sprintf("\t%s\n", err.Error())
+			}
+			res += fmt.Sprintf("%s\n", html)
+		}
 		quads, err := dom.GetContentQuads().WithNodeID(node.NodeID).Do(ctx)
 		if err != nil {
 			res += fmt.Sprintf("\t%s\n", err.Error())
@@ -50,7 +61,7 @@ func (e *ElementAnalysis) getElementHeights(ctx context.Context) error {
 	}
 	err := ioutil.WriteFile(e.filename, []byte(res), 0o644)
 	if err != nil {
-		return err
+		return errors.New("write file " + e.filename + ": " + err.Error())
 	}
 	return nil
 }
@@ -61,9 +72,9 @@ func main() {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 	var url string
-	e := ElementAnalysis{
+	/*e := ElementAnalysis{
 		headerNodes: []*cdp.Node{},
-	}
+	}*/
 
 	flag.StringVar(&url, "url", defaultURL, usageURL)
 	flag.StringVar(&url, "u", defaultURL, usageURL+" (shorthand)")
@@ -72,64 +83,104 @@ func main() {
 	fmt.Printf("Open URL %s\n", url)
 	var ids []cdp.NodeID
 	err := chromedp.Run(ctx,
+		emulation.SetUserAgentOverride("JT-WebScraper-1.0"),
 		// set viewport
 		chromedp.EmulateViewport(780, 651),
 		chromedp.Navigate(url),
 		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
-		chromedp.ActionFunc(func(c context.Context) error {
-			e.filename = "780x651.dom"
-			// depth -1 for the entire subtree
-			// do your best to limit the size of the subtree
-			return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
-		}),
-		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
-		chromedp.ActionFunc(e.getElementHeights),
-		// set viewport
-		chromedp.EmulateViewport(1280, 720),
-		chromedp.Navigate(url),
-		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
-		chromedp.ActionFunc(func(c context.Context) error {
-			e.filename = "1280x720.dom"
-			// depth -1 for the entire subtree
-			// do your best to limit the size of the subtree
-			return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
-		}),
-		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
-		chromedp.ActionFunc(e.getElementHeights),
-		// set viewport
-		chromedp.EmulateViewport(1900, 1280),
-		chromedp.Navigate(url),
-		chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
-		chromedp.ActionFunc(func(c context.Context) error {
-			e.filename = "1900x1280.dom"
-			// depth -1 for the entire subtree
-			// do your best to limit the size of the subtree
-			return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
-		}),
-		chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
-		chromedp.ActionFunc(e.getElementHeights),
-		chromedp.ActionFunc(func(ctx context.Context) error {
+		/*		chromedp.ActionFunc(func(c context.Context) error {
+					e.filename = "780x651.dom"
+					// depth -1 for the entire subtree
+					// do your best to limit the size of the subtree
+					//return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
+					return nil
+				}),
+				chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
+				chromedp.ActionFunc(e.getElementHeights),
+				// set viewport
+				chromedp.EmulateViewport(1280, 720),
+				chromedp.Navigate(url),
+				chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
+				chromedp.ActionFunc(func(c context.Context) error {
+					e.filename = "1280x720.dom"
+					// depth -1 for the entire subtree
+					// do your best to limit the size of the subtree
+					//return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
+					return nil
+				}),
+				chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
+				chromedp.ActionFunc(e.getElementHeights),
+				// set viewport
+				chromedp.EmulateViewport(1900, 1280),
+				chromedp.Navigate(url),
+				chromedp.NodeIDs("body", &ids, chromedp.ByQuery),
+				chromedp.ActionFunc(func(c context.Context) error {
+					e.filename = "1900x1280.dom"
+					// depth -1 for the entire subtree
+					// do your best to limit the size of the subtree
+					//return dom.RequestChildNodes(ids[0]).WithDepth(-1).Do(c)
+					return nil
+				}),
+				chromedp.Nodes(":is(div, a, form, img, li, h1, h2, h3)", &e.headerNodes, chromedp.ByQueryAll),
+				chromedp.ActionFunc(e.getElementHeights),
+				chromedp.ActionFunc(func(ctx context.Context) error {
+					var err error
+					fmt.Println("---------------------")
+					fmt.Println("Get body outerHTML")
+					fmt.Println("---------------------")
+
+					// export html for debug
+					res, err := dom.GetOuterHTML().WithNodeID(ids[0]).Do(ctx)
+					if err != nil {
+						return errors.New("get outer html from node: " + err.Error())
+					}
+					h := sha1.New()
+					h.Write([]byte(res))
+					bs := h.Sum(nil)
+					sha := fmt.Sprintf("%x", bs)
+					err = ioutil.WriteFile(sha+".html", []byte(res), 0o644)
+					if err != nil {
+						return errors.New("save layout " + sha + ".html: " + err.Error())
+					}
+					return nil
+				}),
+		*/chromedp.ActionFunc(func(ctx context.Context /*h cdptypes.Handler*/) error {
 			var err error
-			//			var root *cdp.Node
+			/*root, err := h.GetRoot(ctx)
+			if err != nil {
+				return errors.New("failed to get root node: " + err.Error())
+			}
+			traverse(ctx, root)*/
+
 			fmt.Println("---------------------")
-			fmt.Println("GetDocument")
+			fmt.Println("Get body outerHTML after modification")
 			fmt.Println("---------------------")
-			/*			root, err = dom.GetDocument().Do(ctx)
+			node, err := dom.DescribeNode().WithNodeID(ids[0]).Do(ctx)
+			if err != nil {
+				return errors.New(fmt.Sprintf("error while getting node id %d: %s", ids[0], err.Error()))
+			}
+			err = traverse(ctx, node)
+			/*			root, err := dom.GetDocument().Do(ctx)
 						if err != nil {
-							return err
-						}*/
-			// export html for debug
+							return errors.New(fmt.Sprintf("error while getting dom root: %s", err.Error()))
+						}
+						err = traverse(ctx, root)*/
+
+			if err != nil {
+				return errors.New("failed traverse dom: " + err.Error())
+			}
+
 			res, err := dom.GetOuterHTML().WithNodeID(ids[0]).Do(ctx)
 			if err != nil {
-				return err
+				return errors.New("get outer html from node: " + err.Error())
 			}
 			h := sha1.New()
 			h.Write([]byte(res))
 			bs := h.Sum(nil)
-			fmt.Printf("%x\n", bs)
-			err = ioutil.WriteFile("layout.html", []byte(res), 0o644)
+			sha := fmt.Sprintf("%x", bs)
+			err = ioutil.WriteFile(sha+".html", []byte(res), 0o644)
 			if err != nil {
-				return err
+				return errors.New("save layout " + sha + ".html: " + err.Error())
 			}
 			return nil
 		}),
@@ -140,4 +191,29 @@ func main() {
 	}
 
 	fmt.Printf("\nTook: %f secs\n", time.Since(start).Seconds())
+}
+
+func traverse(ctx context.Context, node *cdp.Node) error {
+	var err error
+	err = dom.RequestChildNodes(node.NodeID).WithDepth(-1).Do(ctx)
+	if err != nil {
+		return errors.New(fmt.Sprintf("error while requesting child nodes from node id %d: %s", node.NodeID, err.Error()))
+	}
+	fmt.Printf("[%d] %d %s %s %d %+v\n", node.NodeID, node.NodeType, node.NodeName, node.NodeValue, node.ChildNodeCount, node.Children)
+	nodeParams, err := dom.DescribeNode().WithNodeID(node.NodeID).WithDepth(1).WithPierce(true).Do(ctx)
+	if err != nil {
+		return errors.New(fmt.Sprintf("error while describing node id %d: %s", node.NodeID, err.Error()))
+	}
+	/*	err = dom.RequestChildNodes(nodeParams.NodeID).WithDepth(-1).Do(ctx)
+		if err != nil {
+			return errors.New(fmt.Sprintf("error while requesting child nodes from node id %d: %s", node.NodeID, err.Error()))
+		}*/
+	for _, n := range nodeParams.Children {
+		//for _, n := range node.Children {
+		err = traverse(ctx, n)
+		if err != nil {
+			return errors.New(fmt.Sprintf("error while traversing child node %d from node %d: %s", n.NodeID, node.NodeID, err.Error()))
+		}
+	}
+	return nil
 }
